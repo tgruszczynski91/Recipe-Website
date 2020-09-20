ed14178d71d704b8a95a2bded03f0cfbbdd454a5
tgruszczynski91 <60708472+tgruszczynski91@users.noreply.github.com>
2020-09-20 16:20:17 +0100

    Fixing navbar issue

--- recipe.js
+++ recipe.js
@@ -3,16 +3,12 @@
 const input = document.querySelector('#counter');
 const li = document.querySelectorAll('.count');
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.menu-ul');
-const navlinks = document.querySelectorAll('.menu-ul li')
+const nav = document.querySelector('.menu-ul')
 
 //Navbar toggle
 burger.addEventListener('click', function(){
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle')
-    navlinks.forEach(function(link){
-        link.style.opacity = 1;
-    })
 })
 
 //Ingridients counter
